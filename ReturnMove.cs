5c785a9e31eb4651bcac51c53445f38e84b67700
lime <utida7497@gmail.com>
2026-04-23 15:49:55 +0900

    Add files via upload

---
+++ ReturnMove.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections.Generic;
+
+public class ReturnMove : MonoBehaviour
+{
+    private Vector2 startPosition;  //èâä˙à íu
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StepBack()
+    {
+        if (BlockMove.isJustMoved || BlockMove.isMoving || BlockMove.moveHistory.Count == 0)
+        {
+            //isJustMoved = false;
+            return;
+        }
+
+        if ((Vector2)transform.position == startPosition) return;
+
+        int lastIndex = BlockMove.moveHistory.Count - 1;
+        Vector2 lastDir = BlockMove.moveHistory[lastIndex];
+
+        Vector2 BackDir = -lastDir;
+        Vector2 BackPos = (Vector2)transform.position + BackDir;
+
+        Collider2D myCollider = GetComponent<Collider2D>();
+        if (myCollider != null) myCollider.enabled = false;
+
+        //[èåèÇP]
+        Collider2D hit = Physics2D.OverlapCircle(BackPos, 0.4f, obstacleLayer);
+
+        if (myCollider != null) myCollider.enabled = true;
+
+        if (hit == null)
+        {
+            BlockMove.isMoving = true;
+
+            BlockMove.moveHistory.RemoveAt(lastIndex);
+
+            transform.DOMove(BackPos, 0.4f)
+                .SetEase(Ease.OutQuad)
+                .OnComplete(() =>
+                {
+                    BlockMove.isMoving = false;
+
+                    if (BlockMove.moveHistory.Count == 0)
+                    {
+                        transform.position = startPosition;
+                    }
+                    var manager = Object.FindAnyObjectByType<TurnManager>();
+                    if (manager != null)
+                    {
+                        manager.CheckAllGoals();
+                    }
+                });
+        }
+        else
+        {
+            BlockMove.isMoving = false;
+            Debug.Log($"{name} : ñﬂÇËêÊÇ™ç«Ç™Ç¡ÇƒÇ¢ÇÈÇΩÇﬂë“ã@ÇµÇ‹Ç∑ÅB");
+        }
+    }
+}
