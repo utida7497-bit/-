a424d311cf2b63a6911e77031f608f044af73a7a
lime <utida7497@gmail.com>
2026-04-27 16:59:52 +0900

    Add files via upload

---
+++ Ana.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections.Generic;
+
+public class Ana : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }// Update is called once per frame
+    void Update()
+    {
+
+    }
+}
